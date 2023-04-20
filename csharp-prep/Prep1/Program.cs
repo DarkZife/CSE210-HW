6ba66ed90fff2adbc7230962d587050103f10745
DarkZife <darkzife@byui.edu>
2023-04-20 17:30:57 -0500

    Changed code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name");
+        String FName = Console.WriteLine();
+        Console.WriteLine("What is your last name?");
+        String LName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LName}, {FName} {LName})
     }
 }
