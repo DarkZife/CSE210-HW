550328175c11f967e0ed68aee24cf656cdff0f22
DarkZife <darkzife@byui.edu>
2023-04-20 17:58:32 -0500

    Made Changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string GradeInText = Console.ReadLine();
+        int Grade = int.Parse(GradeInText);
+
+        string letter = "";
+
+        if (Grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (Grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (Grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (Grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter is: {letter}");
+
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed, better luck next time.");
+        }
+
+
     }
 }
