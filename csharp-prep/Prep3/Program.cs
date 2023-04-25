f23b4f45388ec8868b0f5e5e25786f32ba8547fe
DarkZife <darkzife@byui.edu>
2023-04-24 23:14:03 -0500

    Finished assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magNum = randomGenerator.Next(1, 100);
+
+            int num = -1;
+            int guess = 0;
+
+
+            while (num != magNum)
+            {
+
+                Console.WriteLine("What is your guess? ");
+                num =int.Parse( Console.ReadLine());
+
+                guess += 1;
+
+                if (num == magNum)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Number of guesses {guess}");
+                }
+                else if (num < magNum)
+                {
+                    Console.WriteLine("Higher");
+                }      
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+         
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        }         
     }
+
 }
