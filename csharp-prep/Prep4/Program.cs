4c94ad6a536c0046cc9be410b7ee91cff196a78a
DarkZife <darkzife@byui.edu>
2023-04-25 15:35:08 -0500

    Still need to work on stretch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> listNum = new List<int>();
+
+        int num = -1;
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished");
+
+        while (num != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num != 0)
+            {
+                listNum.Add(num); 
+            }
+
+        }
+
+        int count = 0;
+        int sum = 0;
+
+        foreach (int x in listNum)
+        {
+            Console.WriteLine(x);
+            count += 1;
+            sum += x;
+        }
+
+        int maxNum = listNum.Max();
+
+        float avg = ((float)sum)/count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"Largest number is: {maxNum}");
+
     }
 }
