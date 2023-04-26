92cb068fa964f3c79fc6b6fc780a521d582d2278
DarkZife <darkzife@byui.edu>
2023-04-25 22:33:10 -0500

    updated hw

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        DisplayWelcome();
+        PromptUserName();
+        string userName = Console.ReadLine();
+        int num = PromptUserNumber();
+        int sqrNum = SquareNumber(num);
+
+        DisplayResults(userName, sqrNum);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+
+    }
+    
+    static void PromptUserName()
+    {
+        Console.WriteLine("Enter in your user name: ");
+        
+
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Enter in a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+
+        int squared = num * num;
+
+        return squared;
+
+    }
+
+    static void DisplayResults(string name, int sqrNum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sqrNum}.");
+    }
+
+
 }
