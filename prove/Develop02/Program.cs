4eba81bd495910defb25b9e0a225b61581fb6a86
DarkZife <darkzife@byui.edu>
2023-05-12 01:07:57 -0500

    code finished

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool runProgram = true;
+        
         List<string> prompts = new List<string>()
         {
             "Who was the most interesting person I interacted with today?",
@@ -27,38 +29,49 @@ class Program
             return prompts[i];
         }
 
+        while (runProgram == true)
+        {
+            DisplayMenu();
+            int menu = int.Parse(Console.ReadLine());
 
-        DisplayMenu();
-        int menu = int.Parse(Console.ReadLine());
+            if (menu == 1)
+            {
+                string prompt = GetRandomPrompt(prompts);
+                Console.WriteLine($"{prompt}");
+                string input = Console.ReadLine();
 
-        if (menu == 1)
-        {
-            string prompt = GetRandomPrompt(prompts);
-            Console.WriteLine($"{prompt}");
-            string entry = Console.ReadLine();
-        }
-        else if (menu == 2)
-        {
-            MyJournal.DisplayJournal();
-            return;
-        }
-        else if (menu == 3)
-        {
-            Console.WriteLine("Enter a name to save the file: ");
-            string saveName = Console.ReadLine();
-            MyJournal.Save(saveName);
-            return;
-        }
-        else if(menu == 4)
-        {
-            Console.WriteLine("Enter a entry name to load: ");
-            string LoadName = Console.ReadLine();
-            MyJournal.Load(LoadName);
-            return;
-        }
-        else
-        {
-            return;
+                Entry Addentry = new Entry(prompt, input);
+
+                MyJournal.AddEntry(Addentry);
+
+
+            }
+            else if (menu == 2)
+            {
+                MyJournal.DisplayJournal();
+            
+
+            }
+            else if (menu == 3)
+            {
+                Console.WriteLine("Enter a name to save the file: ");
+                string saveName = Console.ReadLine();
+                MyJournal.Save(saveName);
+            
+            }
+            else if(menu == 4)
+            {
+                Console.WriteLine("Enter a entry name to load: ");
+                string LoadName = Console.ReadLine();
+                MyJournal.Load(LoadName);
+            
+
+ 
+            }
+            else
+            {   
+                runProgram = false;
+            }
         }
     
     static void DisplayMenu()
@@ -80,31 +93,78 @@ class Program
 
 class Entry
 {
+    public string Prompt { get; set; }
+    public string Input { get; set; }
 
+
+    public Entry(string prompt, string input)
+    {
+
+        Prompt = prompt;
+        Input = input;
+
+    }
 
 }
+
+
+
+
 
 class Journal
 {
 
+    List<Entry> entries;
+
     public Journal()
     {
+        entries = new List<Entry>();
 
+    }
+
+    public void AddEntry(Entry Addentry)
+    {
+        entries.Add(Addentry);
     }
 
     public void Save(string SaveName)
     {
+        using (StreamWriter write = new StreamWriter(SaveName))
+        {
+            foreach (Entry Addentry in entries)
+            {
+                write.WriteLine($"{Addentry.Prompt}|{Addentry.Input}");
+            }
+        }
 
     }
 
     public void Load(string LoadName)
     {
+        entries.Clear();
+        using(StreamReader read = new StreamReader(LoadName))
+        {
+            while(!read.EndOfStream)
+            {
+                string line = read.ReadLine();
+                string[] parts = line.Split('|');
+                Entry Addentry = new Entry(parts[0], parts[1]);
+                
+                entries.Add(Addentry);
+            }
+        }
 
     }
     
     
     public void DisplayJournal()
     {
+        foreach (Entry Addentry in entries)
+        {
+            Console.WriteLine($"{Addentry.Prompt}");
+            Console.WriteLine($"{Addentry.Input}");
+            Console.WriteLine();
+        }
 
     }
 
