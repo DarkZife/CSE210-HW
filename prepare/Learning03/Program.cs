fc2e5816175b71eda7cc77e6d7a5b03f45729463
DarkZife <darkzife@byui.edu>
2023-05-16 21:50:09 -0500

    finished and did random testing

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,5 +7,32 @@ class Program
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+
+        //These are fun test ones.
+        Fraction test1 = new Fraction(906478);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        Fraction test2 = new Fraction(9045604,  54848940);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+        Fraction test3 = new Fraction(8405621, 804894560);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
     }
 }
