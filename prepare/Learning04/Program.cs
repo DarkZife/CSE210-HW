3cc865a88bc063f27e4f87d92e151715770b11dd
DarkZife <darkzife@byui.edu>
2023-05-30 19:25:46 -0500

    homework done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment one = new Assignment("samuel Bennett", "Multiplication");
+        Console.WriteLine(one.GetSummary());
+
+
+        MathAssignment two = new MathAssignment("Roberto Rodiguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(two.GetSummary());
+        Console.WriteLine(two.GetHomeworkList());
+
+        WritingAssignment three = new WritingAssignment("Mary Waters", "European History","The Causes of World War II");
+        Console.WriteLine(three.GetSummary());
+        Console.WriteLine(three.GetWritingInformation());
     }
 }
